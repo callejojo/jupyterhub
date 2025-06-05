53b136148c57cc168f31194285a2e6cbc50f08ee
callejojo <gbusuoli@gmail.com>
2025-06-05 16:38:59 +0200

    Update jupyterhub_config.py

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,6 +1,14 @@
 # Configurazione base
 c.JupyterHub.bind_url = 'http://0.0.0.0:8000'
 
+# Configurazione spawner per ambienti limitati
+c.Spawner.default_url = '/lab'  # Usa JupyterLab invece di Notebook
+c.Spawner.args = ['--NotebookApp.allow_origin=*']
+
+# Memoria limitata
+c.Spawner.mem_limit = '512M'
+c.Spawner.cpu_limit = 1
+
 # Autenticazione PAM (usa utenti sistema)
 c.JupyterHub.authenticator_class = 'jupyterhub.auth.PAMAuthenticator'
 
@@ -13,5 +21,9 @@ c.Authenticator.allowed_users = {'admin', 'giorgio'}
 # Permetti creazione utenti via web
 c.JupyterHub.admin_access = True
 
-# Spawner locale
-c.JupyterHub.spawner_class = 'jupyterhub.spawner.LocalProcessSpawner'
+# Timeout più lunghi
+c.Spawner.start_timeout = 120  # 2 minuti invece di 30 secondi
+c.Spawner.http_timeout = 60
+
+# Configurazione spawner per container
+c.JupyterHub.spawner_class = 'jupyterhub.spawner.SimpleLocalProcessSpawner'
